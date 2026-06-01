c976c16b3c77a27de0d09af9a7445b91985ea327
Ibrar Ahmad <ibrarahmadkhan09@gmail.com>
2026-06-01 10:34:25 +0300

    Initial commit: Eureka Server base setup

---
+++ src/main/java/com/shopsphere/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.shopsphere.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // <-- Add this annotation
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
